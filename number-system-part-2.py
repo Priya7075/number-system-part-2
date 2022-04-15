e8dc3058a905cf4bb145c400ed1ad286e0696763
Priya7075 <102401793+Priya7075@users.noreply.github.com>
2022-04-15 12:45:06 +0530

    Add files via upload

---
+++ number-system-part-2.py
@@ -0,0 +1,35 @@
+from fractions import Fraction as F
+# print(F(6,2))#3
+# print(F(2.5))#5/2
+# print(F('2.5'))#5/2
+# print(F("2.5"))#5/2
+# print(F(4.5))#9/2
+# print(F(5,0))#0
+# # b=5/0 # DivisionError
+# v=F(5,20)
+# print(v)#9/2
+# g=2+(g,type(g))#complex
+# print(g.real)#2.0
+# print(g.imag)#3.0
+# h=7*2.33j
+# printj
+# print(h,type(h))#complex 16.099999999j
+# print(h.real)#0.0
+# print(h.imag)#16.099999999
+# print(1e0)#1.0
+# print(4e0)#4.0
+# print(1e1)#10.0
+# print(1e2)#100.0
+# print(12e2)#1200.0
+# print(1.4e0)#1.4
+# print(1.4e1)#14.0
+# print(1.4e2)#140.0
+# print(1.4e3)#1400.0
+# print(4*5)#20
+# print(2**3)#8
+# print(2**4)#16
+# print(1.5**2)#2.25
+# print(4**3)#64
+import math as m
+print(m.exp(3))#e=2.71828
+print(m.exp(1))
